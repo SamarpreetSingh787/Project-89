6de19c4672626b09b248fbfb0c2902986e988f4b
SamarpreetSingh787 <78362149+SamarpreetSingh787@users.noreply.github.com>
2021-03-10 08:24:30 -0800

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,8 +1,30 @@
-function strt() {
-    player1_n=document.getElementById("player1_input").value;
-    player2_n=document.getElementById("player2_input").value;
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
 
-    localStorage.setItem("player1_name",player1_n);
-    localStorage.setItem("player2_name",player2_n);
-    window.location="quiz.html";
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_ques").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_ans").innerHTML = "Answer Turn - " + player2_name;
+
+function send() {
+    no1 = document.getElementById("no1").value;
+    no2 = document.getElementById("no2").value;
+    actual_ans = parseInt(no1) * parseInt(no2);
+    console.log("Actual answerr = " + actual_ans);
+
+    ques_n = "<h4>" + no1 + "X" + no2 + "<h4>"; 
+    ipt_bx = "<br>Answer: <input type='text' id='ipt_word' placeholder='Enter your answer here'>";
+    check_btn = "<br><br> <button class='btn btn-info' onclick='check()'>Check</button>";
+    row = ques_n + ipt_bx + check_btn;
+    document.getElementById("output").innerHTML = row;
+    
+    document.getElementById("no1").value = "";
+    document.getElementById("no2").value = "";
 }
